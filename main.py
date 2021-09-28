f4c32ecdd17a1a33f7ea884ba16c9cd1f99095ab
etud user <etud@port-dell-precision-7520-29>
2021-09-28 16:07:01 +0200

    base version

---
+++ main.py
@@ -0,0 +1,51 @@
+from random import randrange
+
+def main():
+    # generate the solution ??
+
+    # size for the byte slice 
+    n = 8
+
+    # initialize the array, 0 item
+    slice = [0] * n
+
+
+def generate_population(size):
+    """
+        @param size: size of the population at the beggining
+        @return slices: array of byte arrays that are possible solutions 
+    """
+
+def best_offsprings(slices):
+    """
+        This 
+        @param slices: array of offspings byte arrays
+    """
+
+
+def flip(slice, n):
+    """
+        A mutation method for the onemax problem
+        @param slice: a byte array (0 or 1)
+        @param n: number of flip
+    """
+
+    # mutation probability
+
+    for i in n:
+        target = randrange(len(slice) - 1)
+
+        # should we check something here 
+        slice[target] = 1
+
+    return slice
+
+
+# def generate_offsprings():
+    # generate the offspring by using the given the parents 
+
+
+# def print_solution():
+    
+
+# def 
