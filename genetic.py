d5efe9783a563ff0cf9a7ce2f8a6526d6a224063
sitedevente <martinjarod17@gmail.com>
2021-11-02 02:50:10 +0100

    Added base genetic algorithm and base onemax implementation
    Still need to make it more easily customizable

---
+++ genetic.py
@@ -0,0 +1,108 @@
+from abc import abstractmethod, abstractproperty
+
+from chromosomes import Chromosome
+
+class GeneticAlgorithm():
+    """ Template for building generic genetic algorithm based solution"""
+
+    def __init__(
+        self,
+        fixed_size: int = 1,
+    ) -> None:
+
+        """ Initialize the algorithm parameters
+
+            Args:
+                max_size -> max numbers of retained solutions
+                chromosome -> A class with many method important for handling a chromosome
+        """
+
+        self.population: list[Chromosome] = []
+
+        # population fixed size -> maximum and minimum
+        self.fixed_size = fixed_size
+        
+        # cycle
+        self.cycle = 1
+
+        # Last cycle the best was updated
+        # must think of another name
+        self.last_update = 1
+        self.best_chromosome = None
+
+    def __str__(self) -> str:
+        cycle = f"Cycle number: {self.cycle}"
+        fixed_position = f"Fixed size: {self.fixed_size}"
+        current_position_label = "Current population:"
+
+        population_string = ""
+        
+        for chromosome in self.population:
+            population_string = f"{population_string}<--------------------------------->\n{chromosome.__str__}\n"
+        
+        return f"{cycle}\n{fixed_position}\n{current_position_label}\n{population_string}"
+
+    @abstractproperty
+    def should_be_stopped(self) -> bool :
+        """ Decide if the algorithm should be stopped or not"""
+          
+    @abstractmethod
+    def random_sub_population(self):
+        """ Create a population of random chromosomes (solutions)"""
+
+    @abstractmethod
+    def mutate(self):
+        pass
+
+    @abstractmethod
+    def crossover(self):
+        pass
+
+    @abstractmethod
+    def fit(self):
+        """ Give each solution a score so that they can be compared to each other"""
+
+    @abstractmethod
+    def survival(self):
+        """ Introduce a survival mechanism to get rid of some chromosome
+        
+            Also serve as a way to make the population a fixed size
+        """
+
+    def __update_best_chromosome(self):
+        # If it is the first time the function is called then the first chromosome is the best           
+        if self.best_chromosome is None:
+            self.best_chromosome = self.population[0]
+            self.last_update = self.cycle
+
+        for chromosome in self.population:
+            if self.best_chromosome is not chromosome and chromosome.fitness > self.best_chromosome.fitness:
+                self.best_chromosome = chromosome
+                self.last_update = self.cycle
+
+    def new_cycle(self):
+        """ Increase the age of each chromosome in the population"""
+        self.cycle+=1
+
+        for chromosome in self.population:
+            chromosome.inc_age()
+
+    def run(self):
+        self.random_sub_population()
+        self.fit()
+        self.__update_best_chromosome()
+
+        while (not self.should_be_stopped):
+            # if mutation probability is met for the chromosome 
+            self.mutate()
+
+            # if crossover probability is met
+            self.crossover()
+            
+            # prepare for survival
+            self.fit()
+            self.survival()
+
+            # next cycle
+            self.new_cycle()
+            self.__update_best_chromosome()
---
+++ onemax.py
@@ -0,0 +1,88 @@
+from random import randint, random
+from genetic import Chromosome, GeneticAlgorithm
+
+from mutations import FlipFactory, Mutation
+
+
+class OneMax(GeneticAlgorithm):
+
+    def __init__(self, fixed_size: int = 1, chromosome_size:int = 8) -> None:
+        super().__init__(fixed_size=fixed_size)
+        self.chromosome_size = chromosome_size
+        self.mutator: Mutation = FlipFactory.create_flip_mutation(1)
+
+    @property
+    def should_be_stopped(self) -> bool :      
+        MAX_CYCLE = 200
+        MAX_CYCLE_WITHOUT_IMPROVEMENT = 30
+        goal_matched = lambda fitness : fitness == self.chromosome_size
+
+        if MAX_CYCLE > self.cycle:
+            return True
+
+        if self.cycle - self.last_update > MAX_CYCLE_WITHOUT_IMPROVEMENT:
+            return True
+
+        for chromosome in self.population:
+            if chromosome.fitness is not None and goal_matched(chromosome.fitness):
+                return True
+
+        return False
+
+    def __generate_random_chromosome(self):
+        alleles = []
+        for _ in range(self.chromosome_size):
+            number = randint(0,1)
+            alleles.append(number)
+
+        return Chromosome(alleles)
+
+    def random_sub_population(self):
+        """ Generate a list of size number of value of either 0 or 1"""
+        for _ in self.fixed_size:
+            chromosome = self.__generate_random_chromosome()
+            self.population.append(chromosome)
+
+    def mutate(self):
+        mutation_probability: float
+        
+        # Only mutate if cycled based mutation probability occurs
+        # Probability is mutation is high in the early cycle
+        HIGH_MUTATION_PHASE = 75
+        HIGH_MUTATION_PHASE_PROBABILITY = 0.75
+        LOW_MUTATION_PHASE_PROBABILITY = 0.30
+
+        # calculate mutation probability using cycle number
+        if HIGH_MUTATION_PHASE > self.cycle:
+            mutation_probability = HIGH_MUTATION_PHASE_PROBABILITY
+        else:
+            mutation_probability = LOW_MUTATION_PHASE_PROBABILITY
+
+        probability_proportional_to_fitness = lambda fitness: (1 / fitness + 1) * mutation_probability > random()
+
+        # get a random number and compare it to mutation probability 
+        for chromosome in self.population:
+            if probability_proportional_to_fitness(chromosome.fitness):
+                self.mutator.mutate(chromosome)
+
+    def crossover(self):
+        """Do a uniform crosover."""
+        # If probability is met then do a crossover
+
+        # select best parent
+
+        # do a crossover
+
+        # clean parent
+    
+    def fit(self):
+        """ Count the number of 1 """
+        
+        for chromosome in self.population:
+            if chromosome.fitness is None:
+                number_of_one = [number for number in self.chromosome if number is 1]
+                chromosome.fitness = len(number_of_one)
+
+    def survival(self):
+        # calculate probability of survival using         
+        pass
